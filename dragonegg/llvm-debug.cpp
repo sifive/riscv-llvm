f405931050ea4d18f8967f29ad31805568c2b1c9
Duncan Sands <baldrick@free.fr>
2010-01-05 07:03:39 +0000

    Port commit 91353 (dpatel) from llvm-gcc: Check for existing type descriptor in type cache.

--- dragonegg/llvm-debug.cpp
+++ dragonegg/llvm-debug.cpp
@@ -650,9 +650,18 @@ DIType DebugInfo::createStructType(tree type) {
   if (!TypeName.empty())
     FwdName = FwdName + TypeName.data();
   unsigned SFlags = 0;
+  DIDescriptor TyContext =  findRegion(TYPE_CONTEXT(type));
+
+  // Check if this type is created while creating context information 
+  // descriptor. 
+  std::map<tree_node *, WeakVH >::iterator I = TypeCache.find(type);
+  if (I != TypeCache.end())
+    if (MDNode *TN = dyn_cast_or_null<MDNode>(I->second))
+      return DIType(TN);
+  
   llvm::DICompositeType FwdDecl =
     DebugFactory.CreateCompositeType(Tag, 
-                                     findRegion(TYPE_CONTEXT(type)),
+                                     TyContext,
                                      FwdName.c_str(),
                                      getOrCreateCompileUnit(Loc.file), 
                                      Loc.line, 
