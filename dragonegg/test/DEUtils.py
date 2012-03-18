0fe965da17fdbedbd3826fcdf66e60598df384f3
Duncan Sands <baldrick@free.fr>
2012-03-18 17:17:54 +0000

    Only try to compile files for languages gcc was configured with.

---
+++ dragonegg/test/DEUtils.py
@@ -0,0 +1,32 @@
+suffixMap = {
+  '.adb'   : 'ada',
+  '.ads'   : 'ada',
+  '.c'     : 'c',
+  '.i'     : 'c',
+  '.C'     : 'c++',
+  '.cc'    : 'c++',
+  '.cpp'   : 'c++',
+  '.ii'    : 'c++',
+  '.f'     : 'fortran',
+  '.f03'   : 'fortran',
+  '.f08'   : 'fortran',
+  '.f90'   : 'fortran',
+  '.f95'   : 'fortran',
+  '.F'     : 'fortran',
+  '.F03'   : 'fortran',
+  '.F08'   : 'fortran',
+  '.F90'   : 'fortran',
+  '.F95'   : 'fortran',
+  '.go'    : 'go',
+  '.jar'   : 'java',
+  '.class' : 'java',
+  '.m'     : 'objc',
+  '.mm'    : 'objc++',
+}
+
+def getSuffixesForLanguage(language):
+    suffixes = []
+    for suffix in suffixMap:
+       if suffixMap[suffix] == language:
+           suffixes.append(suffix)
+    return suffixes
