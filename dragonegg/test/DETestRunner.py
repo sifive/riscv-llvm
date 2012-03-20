c4914743995e8fdcae607d5ffc1eda60943d32fd
Duncan Sands <baldrick@free.fr>
2012-03-20 15:09:15 +0000

    When compiling Fortran 90 code in parallel it isn't safe to put all output in the same directory.  That's because compiling one file A can create a module needed by another file B.  If B is compiled before A then the compilation of B will fail.  But the real problem is that we compile B several times, each time using a different compiler or different compiler settings.  If A happens to be compiled in the middle of that, sometimes B will compile and sometimes not.  This race can cause spurious test failures.  Work around this by detecting when a compilation has failed due to lack of a module; when this happens, do a "syntax only" compile of everything in sight (this generates modules but not much else) and try again.  This should solve both the "B doesn't compile because A wasn't compiled first" coverage problem, and the race condition, though this does rely on gcc outputting modules in an atomic way (it looks like it tries to do so by writing to a temporary file then renaming it) so it might not work on some filesystems.

--- dragonegg/test/DETestRunner.py
+++ dragonegg/test/DETestRunner.py
@@ -49,6 +49,55 @@ def compareCommands(cmds, args, cwd=None):
     return (Test.FAIL, output.getvalue())
 
 
+def generateFortranModules(cmd, srcPath, OutputDir):
+    # Fortran 90 code often fails to compile because it needs modules defined by
+    # other files in the same directory.  If this seems to be happening then try
+    # to generate all of the required modules by compiling every Fortran file in
+    # the same directory.
+    srcDir,srcBase = os.path.split(srcPath)
+    cmd = cmd + ['-I', srcDir, '-fsyntax-only']
+
+    # If the file compiles OK or isn't failing because of lacking modules then
+    # there is no point in trying to generate modules.
+    out,err,exitCode = TestRunner.executeCommand(cmd + [srcPath], OutputDir)
+    if exitCode == 0 or err is None or "Can't open module file" not in err:
+        return
+
+    # Drat, it fails to compile.  Generate modules for every Fortran file in the
+    # source directory.
+    fortranSuffixes = DEUtils.getSuffixesForLanguage('fortran')
+    filesToCompile = []
+    for filename in os.listdir(srcDir):
+        filepath = os.path.join(srcDir, filename)
+        if not os.path.isdir(filepath):
+            base,ext = os.path.splitext(filename)
+            if ext in fortranSuffixes:
+                filesToCompile.append(filepath)
+
+    # Compile every file, returning triumphantly once the original file manages
+    # to compile, or giving up miserably if no progress is being made.
+    newFilesToCompile = []
+    while filesToCompile != newFilesToCompile:
+        newFilesToCompile = []
+        # Compile each file in turn.
+        for path in filesToCompile:
+            out,err,exitCode = TestRunner.executeCommand(cmd + [path], OutputDir)
+            if exitCode != 0 and err is not None and "Can't open module file" in err:
+                # It failed to compile due to a missing module.  Remember it for
+                # the next round.
+                newFilesToCompile.append(path);
+            elif path == srcPath:
+                # The original file compiled, or at least didn't fail to compile
+                # due to a lacking module.  Return triumphantly!
+                return
+        # Arrange for the next iteration to compile the files that were missing
+        # modules this time round.
+        filesToCompile, newFilesToCompile = newFilesToCompile, filesToCompile
+
+    # The set of files missing modules didn't change, give up miserably.
+    return
+
+
 def executeCompilatorTest(test, litConfig, compilers, flags, language_flags,
                           skip, xfails):
     test_path = '/'.join(test.path_in_suite)
@@ -61,7 +110,6 @@ def executeCompilatorTest(test, litConfig, compilers, flags, language_flags,
     execPath = test.getExecPath()
     execDir,execBase = os.path.split(execPath)
     tmpDir = os.path.join(execDir, 'Output')
-    tmpDir = os.path.join(tmpDir, execBase)
     Util.mkdir_p(tmpDir)
 
     # Is this test expected to fail?
@@ -80,6 +128,11 @@ def executeCompilatorTest(test, litConfig, compilers, flags, language_flags,
     language = DEUtils.getLanguageForSuffix(srcExt)
     if language in language_flags:
       common_args += language_flags[language]
+
+    # Fortran files may not compile because they need modules provided by other
+    # Fortran files.  Workaround this by generating missing modules if possible.
+    if language == 'fortran':
+      generateFortranModules(compilers[0], srcPath, tmpDir)
 
     # Compile the test.
     for args in flags:
