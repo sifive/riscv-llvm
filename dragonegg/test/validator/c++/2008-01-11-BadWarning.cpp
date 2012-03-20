35a6f631e762148c0924b3bfd489c3d4c656a28c
Duncan Sands <baldrick@free.fr>
2012-03-20 20:20:09 +0000

    Not sure why this fails on the buildbot but not here, but presumably due to the |& construct.

--- dragonegg/test/validator/c++/2008-01-11-BadWarning.cpp
+++ dragonegg/test/validator/c++/2008-01-11-BadWarning.cpp
@@ -1,4 +1,4 @@
-// RUN: %dragonegg -xc++ %s -S -o /dev/null |& not grep warning
+// RUN: %dragonegg -xc++ %s -S -o /dev/null 2>&1 | not grep warning
 // rdar://5683899
 void** f(void **Buckets, unsigned NumBuckets) {
   return Buckets + NumBuckets;
