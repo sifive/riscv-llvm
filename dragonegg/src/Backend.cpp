5ea62d911471589e86a1453bae722ab434fbc47f
Bill Wendling <isanbard@gmail.com>
2012-10-09 08:35:52 +0000

    Convert to the new syntax for querying attributes.

--- dragonegg/src/Backend.cpp
+++ dragonegg/src/Backend.cpp
@@ -705,7 +705,7 @@ static void createPerModuleOptimizationPasses() {
     // TODO: Consider letting the GCC inliner do this.
     for (Module::iterator I = TheModule->begin(), E = TheModule->end();
          I != E; ++I)
-      if (I->getFnAttributes().hasAlwaysInlineAttr()) {
+      if (I->getFnAttributes().hasAttribute(Attributes::AlwaysInline)) {
         NeedAlwaysInliner = true;
         break;
       }
