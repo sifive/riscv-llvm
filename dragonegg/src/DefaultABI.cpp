8ccee70e146db06e5c4e320c9039b7272aeb1a7b
Bill Wendling <isanbard@gmail.com>
2012-10-14 18:11:43 +0000

    Try to fix the buildbots by using the correct methods.

--- dragonegg/src/DefaultABI.cpp
+++ dragonegg/src/DefaultABI.cpp
@@ -217,7 +217,7 @@ void DefaultABI::HandleArgument(tree type, std::vector<Type*> &ScalarElts,
         Attributes::Builder B;
         B.addAttribute(Attributes::ByVal);
         B.addAlignmentAttr(LLVM_BYVAL_ALIGNMENT(type));
-        *Attributes |= Attributes::get(B);
+        *Attributes = Attributes::get(B.addAttributes(*Attributes));
       }
     } else {
       C.HandleScalarArgument(Ty, type);
@@ -244,7 +244,7 @@ void DefaultABI::HandleArgument(tree type, std::vector<Type*> &ScalarElts,
         Attributes::Builder B;
         B.addAttribute(Attributes::ByVal);
         B.addAlignmentAttr(LLVM_BYVAL_ALIGNMENT(type));
-        *Attributes |= Attributes::get(B);
+        *Attributes = Attributes::get(B.addAttributes(*Attributes));
       }
     }
   } else if (LLVM_SHOULD_PASS_AGGREGATE_USING_BYVAL_ATTR(type, Ty)) {
@@ -253,7 +253,7 @@ void DefaultABI::HandleArgument(tree type, std::vector<Type*> &ScalarElts,
       Attributes::Builder B;
       B.addAttribute(Attributes::ByVal);
       B.addAlignmentAttr(LLVM_BYVAL_ALIGNMENT(type));
-      *Attributes |= Attributes::get(B);
+      *Attributes |= Attributes::get(B.addAttributes(*Attributes));
     }
   } else if (LLVM_SHOULD_PASS_AGGREGATE_IN_INTEGER_REGS(type, &Size,
                                                         &DontCheckAlignment)) {
