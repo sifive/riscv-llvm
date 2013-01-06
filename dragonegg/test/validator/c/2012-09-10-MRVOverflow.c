2d851dd5a0765adcaf0a131ad96ba8b656b4e5a0
Duncan Sands <baldrick@free.fr>
2013-01-06 17:04:33 +0000

    This tests the 64 bit ABI so isn't relevant on 32 bit.

--- dragonegg/test/validator/c/2012-09-10-MRVOverflow.c
+++ dragonegg/test/validator/c/2012-09-10-MRVOverflow.c
@@ -1,5 +1,6 @@
 // RUN: %dragonegg -S %s -o - | FileCheck %s
 // Check that the MRV code doesn't load from beyond the end of an alloca.
+// XFAIL: i386, i486, i586, i686
 
 struct Three { int a; int b; int c; };
 
