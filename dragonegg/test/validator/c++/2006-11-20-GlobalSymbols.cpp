fd5688e0ce27aad69a134aaef4bf2bf15a74a12f
Duncan Sands <baldrick@free.fr>
2013-01-15 11:49:46 +0000

    Use dragonegg to produce target assembler, not llc, since llc doesn't know enough, eg it will output cfi directives when the assembler doesn't support them.

--- dragonegg/test/validator/c++/2006-11-20-GlobalSymbols.cpp
+++ dragonegg/test/validator/c++/2006-11-20-GlobalSymbols.cpp
@@ -1,4 +1,4 @@
-// RUN: %dragonegg -O0 -g -S %s -o - | llc | %dragonegg -c -xassembler - -o /dev/null
+// RUN: %eggdragon -O0 -g -S %s -o - | %dragonegg -c -xassembler - -o /dev/null
 // PR1013
 // Check to make sure debug symbols use the correct name for globals and
 // functions.  Will not assemble if it fails to.
