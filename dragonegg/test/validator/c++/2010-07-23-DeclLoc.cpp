925a7db49d4d8c6fe2c28793d0335d0c91aec28f
Duncan Sands <baldrick@free.fr>
2013-03-18 10:17:47 +0000

    File names in debug info get an extra layer of indirection since dblaikie's commit 177239.  Adjust this test correspondingly.

--- dragonegg/test/validator/c++/2010-07-23-DeclLoc.cpp
+++ dragonegg/test/validator/c++/2010-07-23-DeclLoc.cpp
@@ -1,9 +1,10 @@
 // RUN: %dragonegg -S -g %s -o - | FileCheck %s
 // Require the template function declaration refer to the correct filename.
 // First, locate the function decl in metadata, and pluck out the file handle:
-// CHECK: {{extract_dwarf_data_from_header.*extract_dwarf_data_from_header.*extract_dwarf_data_from_header.*[^ ]+", metadata !}}[[filehandle:[0-9]+]],
-// Second: Require that filehandle refer to the correct filename:
-// CHECK: {{^!}}[[filehandle]] = metadata {{![{].*}} metadata !"decl_should_be_here.hpp",
+// CHECK: {{extract_dwarf_data_from_header.*extract_dwarf_data_from_header.*extract_dwarf_data_from_header.*[^ ]+", metadata !}}[[filetag:[0-9]+]],
+// Second: Require that filetag refers to metadata holding the correct filename:
+// CHECK: {{^!}}[[filetag]] = metadata !{i32 786473, metadata ![[filedata:[0-9]+]]}
+// CHECK: {{^!}}[[filedata]] = metadata !{metadata !"decl_should_be_here.hpp",
 typedef long unsigned int __darwin_size_t;
 typedef __darwin_size_t size_t;
 typedef unsigned char uint8_t;
