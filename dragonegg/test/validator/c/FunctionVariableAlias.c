c2af95c0276666694aaa83557f51d11f40ff3f33
Duncan Sands <baldrick@free.fr>
2013-05-20 14:22:37 +0000

    Functions are no longer allowed to alias globals (and vice versa) in gcc-4.8.

--- dragonegg/test/validator/c/FunctionVariableAlias.c
+++ dragonegg/test/validator/c/FunctionVariableAlias.c
@@ -1,4 +1,5 @@
 // RUN: %dragonegg -S %s -o - | FileCheck %s
+// XFAIL: gcc-4.8
 
 int qux;
 void foo(void) __attribute__ ((alias ("qux")));
--- dragonegg/test/validator/c/VariableFunctionAlias.c
+++ dragonegg/test/validator/c/VariableFunctionAlias.c
@@ -1,4 +1,5 @@
 // RUN: %dragonegg -S %s -o - | FileCheck %s
+// XFAIL: gcc-4.8
 
 void qux(void) { };
 extern int foo __attribute__ ((alias ("qux")));
