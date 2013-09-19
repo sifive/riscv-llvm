dd5c9b8a48ac27ac0448c532d3ad26e42aa7f11b
Duncan Sands <baldrick@free.fr>
2013-09-19 18:21:32 +0000

    Some linux distributions have patched their system gcc to turn on the stack protector by default.  Ensure this test passes on such systems.

--- dragonegg/test/validator/c/StackProtector.c
+++ dragonegg/test/validator/c/StackProtector.c
@@ -1,5 +1,5 @@
 // RUN: %dragonegg -S -o - %s -fstack-protector --param ssp-buffer-size=1 | FileCheck --check-prefix=SP %s
-// RUN: %dragonegg -S -o - %s | FileCheck --check-prefix=NP %s
+// RUN: %dragonegg -S -o - %s -fno-stack-protector | FileCheck --check-prefix=NP %s
 void foo(void) {}
 // SP: attributes {{.*}} "stack-protector-buffer-size"="1"
 // NP-NOT: stack-protector
