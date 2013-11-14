b196dea83b93167df42bdec8c62dc6201526a1b8
Rafael Espindola <rafael.espindola@gmail.com>
2013-11-14 17:24:02 +0000

    Allow for a bitcast that shows up in some versions of gcc.

--- dragonegg/test/validator/c/ExternFunctionWeakref.c
+++ dragonegg/test/validator/c/ExternFunctionWeakref.c
@@ -3,5 +3,5 @@
 static void function_weakref(void) __attribute__ ((weakref("foo")));
 void *use_function = (void *)function_weakref;
 
-// CHECK: @use_function = unnamed_addr global void ()* @foo
+// CHECK: @use_function = unnamed_addr global {{.*}} @foo
 // CHECK: declare extern_weak void @foo()
